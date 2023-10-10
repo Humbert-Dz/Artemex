c7e9415d96b1f443abb1bfcc9107a1f9b4e6aa6e
Humberto Dominguez <humbertodelac13@gmail.com>
2023-10-09 20:29:30 -0600

    Modulo inicio: corrección en iconos

--- app/Views/artemex/inicio.php
+++ app/Views/artemex/inicio.php
@@ -25,7 +25,7 @@
   <a href="#"
     class="block bg-[#090E1D] bg-account bg-cover w-[300px] h-[200px] rounded-xl p-2 active:scale-95 hover:bg-[#1F2733] shadow-lg shadow-[#3E3E3E]">
     <figure class="w-full h-full flex flex-col justify-evenly items-center">
-      <img src="src/icons/packages.svg" alt="" class="w-1/4" />
+      <img src="src/icons/user.svg" alt="" class="w-1/4" />
       <figcaption class="text-2xl text-center font-black text-white">Mi cuenta</figcaption>
     </figure>
   </a>
